9a54b5c2b2e92ad1ed0dbf515f440b4d00ed5c25
FlutLab.io <>
2021-11-08 15:02:49 +0000

    Initial commit

---
+++ lib/datas/dumdata.dart
@@ -0,0 +1,30 @@
+List<Map<String, dynamic>> dummyUsers = [
+  {
+    'id': 1,
+    'name': 'Heli',
+    'profile': 'https://scontent.fdac68-1.fna.fbcdn.net/v/t39.30808-6/242299907_1209225256246248_2251501776129351681_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=hzmiP6PBbc8AX-q25Cw&tn=IjXNzslXEkzqele5&_nc_ht=scontent.fdac68-1.fna&oh=60e0c57b94cd66f6838647b4bfc8c80e&oe=618DCB1C',
+    'email': 'heli@gmail.com',
+    'position': 5.00
+  },
+  {
+    'id': 1,
+    'name': 'Rahman',
+    'profile': 'https://www.woolha.com/media/2020/03/eevee.png',
+    'email': 'rahman@gmail.com',
+    'position': 4.80
+  },
+  {
+    'id': 1,
+    'name': 'Cabul',
+    'profile': 'https://randomuser.me/api/portraits/men/86.jpg',
+    'email': 'rahman@gmail.com',
+    'position': 4.80
+  },
+  {
+    'id': 1,
+    'name': 'Rahman',
+    'profile': 'https://randomuser.me/api/portraits/men/11.jpg',
+    'email': 'rahman@gmail.com',
+    'position': 4.80
+  }
+];
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+import 'datas/dumdata.dart';
+
+void main() {
+  runApp(MaterialApp(home: FirstPage()));
+}
+
+class FirstPage extends StatefulWidget {
+  _State createState() => _State();
+}
+
+class _State extends State<FirstPage> {
+  final List<Map<String, dynamic>> allUsers = dummyUsers;
+  List<Map<String, dynamic>> copyUsers = [];
+
+  @override
+  void initState() {
+    copyUsers = allUsers;
+    super.initState();
+  }
+
+  void searchData(String value) {
+    List<Map<String, dynamic>> similarUsers = [];
+    if (value.isEmpty) {
+      similarUsers = allUsers;
+    } else {
+      similarUsers = allUsers.where((name) => name['name'].toLowerCase().contains(value.toLowerCase())).toList();
+    }
+    setState(() {
+      copyUsers = similarUsers;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(title: Text('Search View')),
+        body: Padding(
+          padding: EdgeInsets.all(5),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Search Student'),
+                onChanged: (value) => searchData(value),
+              ),
+              Container(
+                  height: 500,
+                  width: double.infinity,
+                  child: copyUsers.isNotEmpty
+                      ? ListView.builder(
+                          itemCount: copyUsers.length,
+                          itemBuilder: (ctx, index) {
+                            return Card(
+                              elevation: 5,
+                              color: Colors.amber,
+                              child: Container(
+                                  height: 130,
+                                  width: double.infinity,
+                                  child: Row(
+                                    children: [
+                                      Expanded(
+                                        flex: 2,
+                                        child: Center(
+                                          child: CircleAvatar(radius: 60, backgroundColor: Colors.brown, backgroundImage: NetworkImage(copyUsers[index]['profile'])),
+                                        ),
+                                      ),
+                                      Expanded(
+                                          flex: 3,
+                                          child: Column(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            crossAxisAlignment: CrossAxisAlignment.center,
+                                            children: [
+                                              Center(
+                                                child: Text(
+                                                  copyUsers[index]['name'],
+                                                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                                                ),
+                                              ),
+                                              Text(
+                                                copyUsers[index]['email'],
+                                                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                                              ),
+                                              CircleAvatar(backgroundColor: Colors.teal, radius: 20, child: Text("${copyUsers[index]['position']}"))
+                                            ],
+                                          ))
+                                    ],
+                                  )),
+                            );
+                          },
+                        )
+                      : Image.network("https://pictures.dealer.com/a/alanjayautomotivenetwork/0193/dfdcaf5815f36d2398fb372b2f294a73x.jpg?impolicy=downsize&w=568"))
+            ],
+          ),
+        ));
+  }
+}
