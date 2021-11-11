179538d20900ea2b4e54c2a464aefca46e14481f
FlutLab.io <>
2021-11-11 08:24:25 +0000

    Autosave

--- lib/thirdpage.dart
+++ lib/thirdpage.dart
@@ -76,6 +76,62 @@ class ThirdPage extends StatelessWidget {
                   );
                 },
               ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              height: 200,
+              width: double.infinity,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: vegetables.length,
+                itemBuilder: (contezt, index) {
+                  return Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                    child: Container(
+                      height: 130,
+                      width: 150,
+                      child: Column(
+                        children: [
+                          Expanded(
+                              flex: 3,
+                              child: Stack(
+                                children: [
+                                  ClipRRect(
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: Image.network(
+                                        vegetables[index]['picture'],
+                                        fit: BoxFit.cover,
+                                        width: 150,
+                                      )),
+                                  Positioned(
+                                      top: 20,
+                                      left: 1,
+                                      child: Container(
+                                        height: 10,
+                                        width: 10,
+                                        color: Colors.white,
+                                        child: Center(
+                                          child: Text('40'),
+                                        ),
+                                      ))
+                                ],
+                              )),
+                          Expanded(
+                            flex: 2,
+                            child: Center(
+                              child: Text(
+                                vegetables[index]['name'],
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
             )
           ],
         ),
