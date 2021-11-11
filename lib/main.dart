b9fae4a389bf1e549f1283cf87b10cbd3393a8a9
FlutLab.io <>
2021-11-11 04:22:11 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class _State extends State<FirstPage> {
 
   @override
   void initState() {
-    allUsers.sort();
     copyUsers = allUsers;
     super.initState();
   }
@@ -25,7 +24,6 @@ class _State extends State<FirstPage> {
   void searchData(String value) {
     List<Map<String, dynamic>> similarUsers = [];
     if (value.isEmpty) {
-      allUsers.sort();
       similarUsers = allUsers;
     } else {
       similarUsers = allUsers.where((name) => name['name'].toLowerCase().contains(value.toLowerCase())).toList();
