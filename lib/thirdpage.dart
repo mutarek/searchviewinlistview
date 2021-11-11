5ded2ce5ea79dcb0a8eace9bb73744bcc2732f91
FlutLab.io <>
2021-11-11 08:05:36 +0000

    Autosave

--- lib/thirdpage.dart
+++ lib/thirdpage.dart
@@ -44,7 +44,7 @@ class ThirdPage extends StatelessWidget {
                                       width: 10,
                                       color: Colors.white,
                                       child: Center(
-                                        child: Text(vegetables[index]['price'].toString()),
+                                        child: Text('{$vegetables[index]['price']}'
                                       ),
                                     ),
                                   )
