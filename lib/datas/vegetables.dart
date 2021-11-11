4669e4e9681bbc1a931585e1ecb1e431fef47d8f
FlutLab.io <>
2021-11-11 08:10:34 +0000

    Autosave

--- lib/datas/vegetables.dart
+++ lib/datas/vegetables.dart
@@ -10,28 +10,23 @@ List<dynamic> vegetables = [
     'picture': 'https://www.transparentpng.com/thumb/tomato/YULABo-tomato-vegetable-transparent.png'
   },
   {
-    'name': 'Carrot',
+    'name': 'Tomato',
     'price': 100,
-    'picture': 'https://freepngimg.com/png/1297-carrot-png-image'
+    'picture': 'https://www.transparentpng.com/thumb/vegetable/fZslFj-vegetable-cabbage-transparent.png'
   },
   {
-    'name': 'Cabbage',
+    'name': 'Broccoli',
+    'price': 150,
+    'picture': 'https://www.transparentpng.com/thumb/tomato/YULABo-tomato-vegetable-transparent.png'
+  },
+  {
+    'name': 'Tomato',
     'price': 100,
-    'picture': 'https://freepngimg.com/png/5509-cabbage-png-image'
+    'picture': 'https://www.transparentpng.com/thumb/vegetable/fZslFj-vegetable-cabbage-transparent.png'
   },
   {
-    'name': 'Red Chili ',
-    'price': 100,
-    'picture': 'https://freepngimg.com/png/3488-red-chili-pepper-png-image'
-  },
-  {
-    'name': 'Patoto',
-    'price': 20,
-    'picture': 'https://freepngimg.com/png/819-potato-png-images-pictures-download'
-  },
-  {
-    'name': 'Cucumber',
-    'price': 40,
-    'picture': 'https://www.transparentpng.com/thumb/vegetable/of4W7J-cucumber-vegetable-transparent-picture.png'
+    'name': 'Broccoli',
+    'price': 150,
+    'picture': 'https://www.transparentpng.com/thumb/tomato/YULABo-tomato-vegetable-transparent.png'
   },
 ];
